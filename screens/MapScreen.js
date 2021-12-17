d610e0963a172216f433dddf30c35761172de57a
DanielDinshtein <danieldinsh@gmail.com>
2021-12-17 22:24:47 +0200

    Leacture 235 - Init Screens & Navigation

---
+++ screens/MapScreen.js
@@ -0,0 +1,17 @@
+import react from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+
+const MapScreen = props => {
+    return(
+        <View>
+            <Text>MapScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default MapScreen;
---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,23 @@
+import react from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+
+const NewPlaceScreen = props => {
+    return(
+        <View>
+            <Text>NewPlaceScreen</Text>
+        </View>
+    );
+}
+
+NewPlaceScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Add Place'
+    };
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default NewPlaceScreen;
