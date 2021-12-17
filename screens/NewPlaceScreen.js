a765f461b0dcdfe6c32bdf7e84ef202b127793d3
DanielDinshtein <danieldinsh@gmail.com>
2021-12-17 23:21:48 +0200

    Leacture 236 - Getting started with Form

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,23 +1,68 @@
-import react from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import {
+    ScrollView,
+    View,
+    Button,
+    Text,
+    TextInput,
+    StyleSheet,
+} from "react-native";
+
+import Colors from "../constants/Colors";
 
 
 const NewPlaceScreen = props => {
-    return(
-        <View>
-            <Text>NewPlaceScreen</Text>
-        </View>
-    );
-}
 
-NewPlaceScreen.navigationOptions = navData => {
+    const [titleValue, setTitleValue] = useEffect('');
+
+    const titleChangedHandler = text => {
+        setTitleValue(text);
+    };
+
+    const savePlaceHandler = () => {
+        
+    };
+
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={titleChangedHandler}
+                    value={titleValue}
+                />
+                <Button
+                    title="Save Place"
+                    color={Colors.primary}
+                    onPress={savePlaceHandler}
+                />
+            </View>
+        </ScrollView>
+    );
+};
+
+NewPlaceScreen.navigationOptions = (navData) => {
     return {
-        headerTitle: 'Add Place'
+        headerTitle: "Add Place",
     };
 };
 
 const styles = StyleSheet.create({
-
+    form: {
+        margin: 30,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15,
+    },
+    textInput: {
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2,
+    },
 });
 
 export default NewPlaceScreen;
